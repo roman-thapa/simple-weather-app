7b3936ee32019797a71483bc7195976c581640ea
roman <51534566+roman-thapa@users.noreply.github.com>
2023-09-04 09:20:44 +0530

    Add input field and log weather data according to input

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("connected")
+const locationInput = document.querySelector('input');
+const search = document.querySelector('#search')
+
+search.addEventListener('click', () => {
+    locationInput.value ?
+    getWeatherData(locationInput.value) :
+    getWeatherData()
+})
+
+const getWeatherData = async (place = "pune") => {
+    try {
+        const rawData = await fetch(`https://api.weatherapi.com/v1/current.json?key=5b009d0552f2431abdd32817232808&q=${place}`)
+        const data = await rawData.json();
+        console.log(data);
+    }
+    catch(err) {
+        throw new Error(err)
+    }
+}
